7e144032c7b49611a452220feb6bede6a1519905
steve <stevepesce879@gmail.com>
2020-07-28 15:39:53 -0400

    hid the login form

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,14 @@
+// hide login div until called
+const loginDiv = document.getElementById('login-container');
+const loginForm = loginDiv.querySelector("form#login-form");
+loginDiv.style.display = "none";
 
-const loginForm = document.getElementById("login-form");
-
+//when login button clicked
 const loginHandler = (e) =>
 {
   e.preventDefault();
   console.log(e.target);
 }
 
+//login handle
 loginForm.addEventListener("submit",e => loginHandler(e));
