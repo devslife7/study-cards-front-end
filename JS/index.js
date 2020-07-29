c395ad2c5d222fb04fb3d02e81e8a460277ef880
steve <stevepesce879@gmail.com>
2020-07-29 18:54:07 -0400

    made new page show cards to fill out, css for them is in testing.css for now

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+
 const USER_URL = 'http://localhost:3000/users/'
 
 const pageDivs = document.getElementsByClassName('page')
@@ -7,7 +8,8 @@ let currentPageDiv
 
 //runner method for this file
 const main = () => {
-  showPage('login')
+  //showPage('login')  
+  renderNewCard();
 
   const loginForm = document.getElementById('login-form')
   loginForm.addEventListener('submit', e => loginHandler(e))
@@ -24,7 +26,7 @@ const showPage = pageName => {
   for (let div of pageDivs) {
     //hide page unless class list includes <pageName>
     if (div.classList.contains(pageName)) {
-      div.style.display = 'initial'
+      div.style.display = 'flex'
       currentPageDiv = div
     } else div.style.display = 'none'
   }
@@ -53,9 +55,59 @@ const renderDashboard = () => {
 const renderNewCard = (course) =>
 {
   showPage("new-card");
-  const cardList = currentPageDiv.querySelector("div#card-list");
-  console.log(cardList);
+  const cardList = currentPageDiv.querySelector("div#new-card-form");
+  cardList.innerHTML = "";
+  for(let i = 1; i < 11; i++)
+  {
+    renderCardRow(cardList,i);
+  }
 
+}
+
+const renderCardRow = (cardList,rowNum) =>
+{
+  //create one row
+  const cardRow = document.createElement("div");
+  cardRow.className = "card-row";
+  cardList.append(cardRow);
+
+  //make a left and right div to hold text and radio
+  const cardLeft = document.createElement('div');
+  const cardRight = document.createElement('div');
+  cardLeft.className = "new card left cell";
+  cardRight.className = "new card right cell"; 
+  cardRow.append(cardLeft,cardRight);
+
+  //make the text boxes
+  const term = document.createElement("textarea");
+  const answ = document.createElement("textarea");
+  term.className = "new card left input element";
+  answ.className = "new card right input element";
+
+  //order for a row is radio,term-text,answer-text,radio
+  addRadioButtons(cardLeft,rowNum,1);
+  cardLeft.append(term);
+
+  cardRight.append(answ);
+  addRadioButtons(cardRight,rowNum,2);
+}
+
+const addRadioButtons = (cardCell,row,column) =>
+{
+  const radioDiv = document.createElement('div');
+  radioDiv.className = "control new card element";
+  radioDiv.innerHTML = 
+  `
+    <label class="radio">
+      <input type="radio" name="latex-${row}-${column}" checked>
+      Text
+    </label><br>
+    <label class="radio">
+      <input type="radio" name="latex-${row}-${column}">
+      LaTeX
+    </label>
+  `;
+  cardCell.append(radioDiv);
 }
 
 //handler for listener added on li's in Course:renderCourse(ul)
