735ed355466d5cec3b487ae009fa981815b210a7
steve <stevepesce879@gmail.com>
2020-07-28 20:05:26 -0400

    added showPage(str) => hides all page divs and shows str div, started dashboard, renders list of courses, adds listeners, listener just console.logs the course object for now

--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,71 @@
 const USER_URL = 'http://localhost:3000/users/'
-// hide login div until called
-const loginDiv = document.getElementById('login-container')
-const loginForm = loginDiv.querySelector('form#login-form')
-// loginDiv.style.display = "none";
-let currentUser
 
-loginForm.addEventListener('submit', e => loginHandler(e))
+const pageDivs = document.getElementsByClassName("page");
 
-const renderLogin = () => {
-  loginDiv.style.display = 'initial'
+let currentUser;
+let currentPageDiv;
+
+//runner method for this file
+const main = () =>
+{
+  showPage("home");
+  const loginForm = document.getElementById("login-form");
+  loginForm.addEventListener('submit', e => loginHandler(e));
 }
+// showPage(string):boolean
+//displays ONLY the divs with the <pageName> class
+//AND sets that page as currentPageDiv
+const showPage = (pageName) =>
+{
+  for(let div of pageDivs)
+  {                     //hide page unless class list includes <pageName>
+    if(div.classList.contains(pageName))
+    {
+      div.style.display = "initial";
+      currentPageDiv = div;
+    }else 
+      div.style.display = "none";
+  }
+}
+
+//show login page div
+const renderLogin = () => {
+  showPage("login");
+}
+
 //when login button clicked
+//submit event
 const loginHandler = e => {
   e.preventDefault()
   const username = e.target.querySelector('input#login-inp').value
-  setUser(username)
+  fetchUserThenRender(username)
 }
 
+//show dashboard page div
+// class="dashboard page"
 const renderDashboard = () =>
 {
-  console.log(currentUser);
+  showPage("dashboard");
+
+  const courseList = currentPageDiv.querySelector("ul#course-list");
+  courseList.innerHTML = "";
+
+  currentUser.courses.forEach(course => 
+    {
+      const li = document.createElement("li");
+      li.innerText = course.name;
+      li.dataset.course_id = course.id;
+      courseList.append(li);
+      li.addEventListener("click",() => courseLiHandler(course))
+    });
 }
 
-const setUser = inp => {
+const courseLiHandler = (courseObj) =>
+{
+  console.log(courseObj);
+}
+
+const fetchUserThenRender = inp => {
   //url to fetch by username if NaN, or search by id if number
   const url = USER_URL + (isNaN(inp) ? `find/${inp}` : inp)
   fetch(url)
@@ -32,3 +75,5 @@ const setUser = inp => {
       renderDashboard()
     })
 }
+ 
+main();
