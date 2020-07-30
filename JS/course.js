ce714d5dfec74e09c9c4095259030795ad49f19d
Marcos Velasco <devslife7@gmail.com>
2020-07-30 00:25:28 -0400

    added better comments to index.html, courses page is now working

--- JS/course.js
+++ JS/course.js
@@ -1,21 +1,49 @@
-class Course
-{
-  constructor(courseJson)
-  {
-    this.id = courseJson.id;
-    this.name = courseJson.name;
-    this.private = courseJson.private;
-    this.cards = courseJson.cards.map(card => {return new Card(card)})
+class Course {
+  constructor(courseJson) {
+    this.id = courseJson.id
+    this.name = courseJson.name
+    this.private = courseJson.private
+    this.cards = courseJson.cards.map(card => {
+      return new Card(card)
+    })
   }
-  //makes an li with this course data and put it on the ul
-  renderCourse(ul)
-  {
-    const li = document.createElement('li');
-    li.innerText = this.name;
-    li.dataset.id = this.id;
 
-    //one listener for every li
-    ul.append(li);
-    li.addEventListener('click', () => courseLiHandler(this));
+  //makes an li with this course data and put it on the ul
+  renderCourse(ul) {
+    const li = document.createElement('li')
+    li.innerText = this.name
+    li.dataset.courseId = this.id
+    ul.append(li)
+
+    this.addCourseListener(li)
+  }
+
+  addCourseListener(li) {
+    li.addEventListener('click', () => {
+      const coursesList = document.getElementById('course-list')
+      // clear previously highlited course
+      for (const course of coursesList.childNodes) {
+        course.classList = ''
+      }
+      li.classList += 'course-list-hover-color'
+
+      const userCoursesCards = document.querySelector('div.user-courses-cards')
+      userCoursesCards.innerHTML = ''
+
+      this.cards.forEach(card => this.renderCard(card))
+    })
+  }
+
+  renderCard(card) {
+    const userCoursesCards = document.querySelector('div.user-courses-cards')
+
+    const cardDiv = document.createElement('div')
+    const cardText = document.createElement('p')
+
+    cardDiv.classList += 'grid-item'
+    cardText.textContent = `${card.card_front}`
+    // render the cards to container
+    cardDiv.appendChild(cardText)
+    userCoursesCards.appendChild(cardDiv)
   }
 }
--- JS/index.js
+++ JS/index.js
@@ -2,24 +2,31 @@ const USER_URL = 'http://localhost:3000/users/'
 
 const pageDivs = document.getElementsByClassName('page')
 
-let currentUser;//User object made from User class
+let currentUser // User object made from User class
 let currentPageDiv
 
-//runner method for this file
+// runner method for this file
 const main = () => {
   showPage('login')
+  addLogInListener()
+  // addCoursesListeners()
+}
 
+const addLogInListener = () => {
   const loginForm = document.getElementById('login-form')
   loginForm.addEventListener('submit', e => loginHandler(e))
-
-  const loginButton = document.getElementById('login-button-nav')
-  loginButton.addEventListener('click', () => {
-    showPage('login')
-  })
 }
+
+// const addCoursesListeners = () => {
+//   const userCourseList = document.getElementById('course-list')
+//   userCourseList.addEventListener('click', e => {
+//     console.log(e.target)
+//   })
+// }
+
 // showPage(string):boolean
-//displays ONLY the divs with the <pageName> class
-//AND sets that page as currentPageDiv
+// displays ONLY the divs with the <pageName> class
+// AND sets that page as currentPageDiv
 const showPage = pageName => {
   for (let div of pageDivs) {
     //hide page unless class list includes <pageName>
@@ -30,15 +37,15 @@ const showPage = pageName => {
   }
 }
 
-//when login button clicked
-//submit event
+// when login button clicked
+// submit event
 const loginHandler = e => {
   e.preventDefault()
   const username = e.target.querySelector('input#login-inp').value
   fetchUserThenRender(username)
 }
 
-//show dashboard page div
+// show dashboard page div
 // class="dashboard page"
 const renderDashboard = () => {
   showPage('dashboard')
@@ -46,25 +53,18 @@ const renderDashboard = () => {
   const courseList = currentPageDiv.querySelector('ul#course-list')
   courseList.innerHTML = ''
 
-  currentUser.renderCourses(courseList);
+  currentUser.renderCourses(courseList)
 }
 
-//user creates new cards for a course
-const renderNewCard = (course) =>
-{
-  showPage("new-card");
-  const cardList = currentPageDiv.querySelector("div#card-list");
-  console.log(cardList);
-
-}
-
-//handler for listener added on li's in Course:renderCourse(ul)
-const courseLiHandler = course => {
-  console.log(course)
+// user creates new cards for a course
+const renderNewCard = course => {
+  showPage('new-card')
+  const cardList = currentPageDiv.querySelector('div#card-list')
+  console.log(cardList)
 }
 
 const fetchUserThenRender = inp => {
-  //url to fetch by username if NaN, or search by id if number
+  // url to fetch by username if NaN, or search by id if number
   const url = USER_URL + (isNaN(inp) ? `find/${inp}` : inp)
   fetch(url)
     .then(r => r.json())
